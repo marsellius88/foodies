8ad93756482dddac321e685ce075019d16faab4c
marsellius88 <marselliustan1@gmail.com>
2025-05-13 15:31:06 +0700

    added server-side input validation and production optimization

--- app/meals/page.js
+++ app/meals/page.js
@@ -21,7 +21,7 @@ export default function MealsPage() {
           Choose your favorite recipe and cook it yourself. It is easy and fun!
         </p>
         <p className={classes.cta}>
-          <Link href="/community">Share Your Favorite Recipe</Link>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
         </p>
       </header>
       <main className={classes.main}>
